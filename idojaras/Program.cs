9fef4650299413c4fc1a3b95a10869d49390bf05
Kökényesi Andor <20.kokenyesi.andor@mechwart.com>
2025-09-30 12:49:16 +0200

    97/100 biro

--- idojaras/Program.cs
+++ idojaras/Program.cs
@@ -36,7 +36,7 @@ class Program
                     }
                     else
                     {
-                        Console.WriteLine("Hibás formátum! Adja meg újra (maxHo minHo):");
+                        throw new Exception("Hibás formátum! Adja meg újra (maxHo minHo):");
                     }
                 }
             })
